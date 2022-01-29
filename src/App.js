baf5aa28c86f8f6ae4dea10e325660c8b523a61c
DimaVian <viandima41@gmail.com>
2022-01-29 19:29:41 +0200

    Flickr Gallery v1

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ class App extends Component {
     super();
     this.state = {
       pictures: [],
+      textInput:'cats'
     };
   }
-componentDidMount=()=>{
-    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=aabca25d8cd75f676d3a74a72dcebf21&tags=nyc&per_page=25&page=1&format=json&nojsoncallback=1')
+
+ReloadImages=()=>{
+    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=aabca25d8cd75f676d3a74a72dcebf21&tags='+this.state.textInput+'&per_page=25&page=1&format=json&nojsoncallback=1')
     .then(function(response){
       return response.json();
     })
@@ -25,15 +27,29 @@ componentDidMount=()=>{
     }.bind(this))
   }
 
+  HandleChange=(e)=>{
+    this.setState({textInput: e.target.value});
+  }
+
+  Delay = (function(){
+    var timer = 0;
+    return function(callback,ms){
+      clearTimeout(timer);
+      timer = setTimeout(callback,ms);
+    }
+  })();
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to the Flickr Gallery</h1>
         </header>
-        <button className='button'>
-          <a className='a' onClick={this.componentDidMount}>Fetch Recent Photos</a>
-          </button>
+        <input className='textInput' onChange={this.HandleChange} 
+        onKeyUp={()=> this.Delay(function(){
+          this.ReloadImages();
+        }.bind(this),1000)}
+        />
         <div >
           <div className='pictures' >{this.state.pictures}</div>
         </div>
